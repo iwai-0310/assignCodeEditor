053ce72d91d2323355274bbf99f67b081104b0ff
iwai-0310 <chazesingh@gmail.com>
2023-10-22 19:36:46 +0530

    Improved upon the cssIndentation

--- script.js
+++ script.js
@@ -34,12 +34,12 @@ function saveData(){
     let datatype= 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
     //Get the content from the left and right textarea
     let saveContentLeft= document.getElementById('left-input').value;
+    console.log(`${saveContentLeft}`);
     let saveContentRight=document.getElementById('right-input').value;
     // check if the content is there in either textarea
     if(saveContentLeft || saveContentRight){
         //Combine the content from both textareas
-        const combinedContent = `${saveContentLeft}\n\n${saveContentRight}`;
-
+        const combinedContent = `${saveContentLeft} \n \n \n${saveContentRight}`;
         // let tableHtmlLeft=saveContentLeft.outerHTML;
         // let tableHtmlRight=saveContentRight.outerHTML;
 
@@ -62,6 +62,9 @@ function saveData(){
         alert('No Content to download ')
     }
 }
+
+
+
 // Function to copy the content of right and left textareas
 function copyData() {
     // Get the content from the left and right textareas
@@ -135,12 +138,45 @@ function toggleLock() {
     }
 }
 // Function for normal indentation
-function Indent(event){
-    if(event.key === 'Enter'){
-        console.log("Enter key pressed");
-        event.preventDefault(); // Prevent the default Enter key behavior
+// function Indent(event){
+//     if(event.key === 'Enter'){
+//         console.log("Enter key pressed");
+//         event.preventDefault(); // Prevent the default Enter key behavior
 
-        const textarea = event.target; // Get the textarea element
+//         const textarea = event.target; // Get the textarea element
+//         const cursorPosition = textarea.selectionStart;
+//         const text = textarea.value;
+
+//         // Find the current line
+//         const lines = text.split('\n');
+//         const currentLineIndex = text.substr(0, cursorPosition).split('\n').length - 1;
+//         const currentLine = lines[currentLineIndex];
+
+//         // Find the current indentation of the line
+//         const indentationMatch = currentLine.match(/^\s*/);
+//         const currentIndentation = indentationMatch ? indentationMatch[0] : '';
+
+//         // Insert the current indentation + tab before the new line
+//         const newLine = '\n' + currentIndentation + '   ';
+//         console.log(newLine);
+//         const newText = text.substring(0, cursorPosition) + newLine + text.substring(cursorPosition);
+//         console.log(newText);
+//         textarea.value = newText;
+
+//         // Adjust the cursor position
+//         textarea.selectionStart = cursorPosition + newLine.length;
+//         textarea.selectionEnd = cursorPosition + newLine.length;
+//     }
+//  }
+
+//Function for HTML indentation
+function HtmlIndent(event) {
+    if (event.key === 'Enter') {
+        // Prevent the default Enter key behavior
+        event.preventDefault(); 
+
+        // Get the textarea element
+        const textarea = event.target; 
         const cursorPosition = textarea.selectionStart;
         const text = textarea.value;
 
@@ -149,20 +185,86 @@ function Indent(event){
         const currentLineIndex = text.substr(0, cursorPosition).split('\n').length - 1;
         const currentLine = lines[currentLineIndex];
 
-        // Find the current indentation of the line
+        // Calculate the current indentation of the line
         const indentationMatch = currentLine.match(/^\s*/);
         const currentIndentation = indentationMatch ? indentationMatch[0] : '';
 
-        // Insert the current indentation + tab before the new line
-        const newLine = '\n' + currentIndentation + '   ';
-        console.log(newLine);
+        // Check if the current line contains a closing tag
+        const isClosingTag = /<\/\w+>$/.test(currentLine);
+
+        // Determine the additional indentation
+        const additionalIndentation = isClosingTag ? '' : '   '; // Three spaces for tab
+
+        // Insert the current indentation + additional indentation before the new line
+        const newLine = '\n' + currentIndentation + additionalIndentation;
         const newText = text.substring(0, cursorPosition) + newLine + text.substring(cursorPosition);
-        console.log(newText);
         textarea.value = newText;
 
         // Adjust the cursor position
         textarea.selectionStart = cursorPosition + newLine.length;
         textarea.selectionEnd = cursorPosition + newLine.length;
     }
- }
+}
+
+// Indentation function to handle the right side textarea containing css element. 
+function CssIndent(event) {
+    if (event.key === 'Enter') {
+        // Get the textarea element
+        const textarea = event.target; 
+        const cursorPosition = textarea.selectionStart;
+        const text = textarea.value;
+
+        // Find the current line
+        const lines = text.split('\n');
+        const currentLineIndex = text.substr(0, cursorPosition).split('\n').length - 1;
+        const currentLine = lines[currentLineIndex];
+
+        // Calculate the current indentation of the line
+        const indentationMatch = currentLine.match(/^\s*/);
+        const currentIndentation = indentationMatch ? indentationMatch[0] : '';
+
+        // Check if the current line contains an opening curly brace
+        const isOpeningCurlyBrace = /{[^}]*$/.test(currentLine);
+
+        // Check if the current line contains a closing curly brace
+        const isClosingCurlyBrace = /}[^}]*$/.test(currentLine);
+
+        // Determine the additional indentation
+        let additionalIndentation = '';
+        if (isOpeningCurlyBrace) {
+            additionalIndentation = '     '; // five spaces
+        } else if (isClosingCurlyBrace) {
+            additionalIndentation = '';
+        }
+
+        // Insert the current indentation + additional indentation before the new line
+        const newLine = '\n' + currentIndentation + additionalIndentation;
+        const newText = text.substring(0, cursorPosition) + newLine + text.substring(cursorPosition);
+
+        // Adjust the cursor position
+        if (isClosingCurlyBrace) {
+            // Move the cursor to the beginning of the next line with no indentation
+            const nextLineIndex = currentLineIndex + 1;
+            const nextLine = lines[nextLineIndex];
+            const nextLineIndentationMatch = nextLine.match(/^\s*/);
+            const nextLineIndentation = nextLineIndentationMatch ? nextLineIndentationMatch[0] : '';
+            const newCursorPosition = cursorPosition + newLine.length - currentIndentation.length + nextLineIndentation.length;
+            textarea.selectionStart = newCursorPosition;
+            textarea.selectionEnd = newCursorPosition;
+        } else {
+            // Default behavior, move the cursor to the end of the new line
+            textarea.selectionStart = cursorPosition + newLine.length;
+            textarea.selectionEnd = cursorPosition + newLine.length;
+        }
+
+        // Update the textarea value
+        textarea.value = newText;
+        event.preventDefault(); // Prevent the default Enter key behavior
+    }
+}
+
+
+
+
+
 
